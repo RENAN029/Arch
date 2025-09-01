f724b66aa1db36a49c1aa32b8a9c866eebb3356f
Renan <pires.s@escolar.ifrn.edu.br>
2025-09-01 15:27:58 -0300

    Adicionando arquivos teste.sh e setup2.sh

---
+++ setup2.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -e
+
+echo "==> Removendo pacotes GNOME indesejados..."
+sudo pacman -Rsnu --noconfirm gnome-extra gnome-music gnome-maps gnome-tour gnome-weather epiphany
+
+echo "==> Instalando pacotes básicos..."
+sudo pacman -S --noconfirm nano git fastfetch gufw noto-fonts-cjk fwupd gnome-tweaks
+
+echo "==> Ativando firewall (GUFW)..."
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw enable
+
+echo "==> Instalando pacotes multimídia..."
+sudo pacman -S --noconfirm ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer
+
+echo "==> Instalando pikaur..."
+sudo pacman -S --needed --noconfirm base-devel git
+git clone https://aur.archlinux.org/pikaur.git
+cd pikaur
+makepkg -fsri --noconfirm
+cd ..
+rm -rf pikaur
+
+echo "==> Instalando Shader Booster..."
+git clone https://github.com/psygreg/shader-patcherx.git
+cd shader-patcherx
+chmod +x patcher.sh
+./patcher.sh
+cd ..
+rm -rf shader-patcherx
+
+echo "==> Instalando LucidGlyph..."
+git clone https://github.com/maximilionus/lucidglyph.git
+cd lucidglyph
+sudo ./lucidglyph.sh install
+cd ..
+rm -rf lucidglyph
+
+echo "==> Configurando /etc/pacman.conf..."
+sudo sed -i 's/^#Color/Color/' /etc/pacman.conf
+sudo sed -i '/Color/a ILoveCandy' /etc/pacman.conf
+sudo sed -i '/^ParallelDownloads/d' /etc/pacman.conf
+sudo sed -i '/ILoveCandy/a ParallelDownloads = 15' /etc/pacman.conf
+echo -e "[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
+
+echo "==> Atualizando sistema e instalando microcódigo Intel..."
+sudo pacman -Syu --noconfirm intel-ucode
+
+echo "==> Script finalizado com sucesso!"
+
