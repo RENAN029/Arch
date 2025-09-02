de3632a8acf2c04dd688abf3133256c44e0ca5f1
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-09-02 13:20:32 -0300

    Update s.sh

--- s.sh
+++ s.sh
@@ -4,11 +4,11 @@ set -e
 echo "==> Removendo pacotes GNOME indesejados..."
 sudo pacman -Rsnu --noconfirm gnome-extra gnome-music gnome-maps gnome-tour gnome-weather gnome-contacts epiphany htop vim
 
+echo "==> Instalando pacotes multimídia..."
+sudo pacman -S --noconfirm ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav
+
 echo "==> Instalando pacotes básicos..."
 sudo pacman -S --noconfirm nano git fastfetch gufw noto-fonts-cjk fwupd gnome-tweaks flatpak
-
-echo "==> Instalando pacotes multimídia..."
-sudo pacman -S --noconfirm ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav 
 
 echo "==> Instalando LucidGlyph..."
 git clone https://github.com/maximilionus/lucidglyph.git
