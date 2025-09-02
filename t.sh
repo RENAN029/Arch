2cc13f794e97ccedde0ebe23e921c14f7c96af79
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-09-02 13:58:28 -0300

    Delete t.sh

--- t.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "==> Atualizando sistema e instalando microcódigo Intel..."
-sudo pacman -Scc --noconfirm
-echo "==> Script finalizado com sucesso; instale o nvm se quiser depois!"
-cd ..
-rm -rf Arch
