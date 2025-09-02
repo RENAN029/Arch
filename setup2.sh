220b8acdf60daf84370c49c0ac9418186f7ec0ef
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-09-02 08:28:20 -0300

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -15,6 +15,14 @@ sudo ufw enable
 echo "==> Instalando pacotes multimídia..."
 sudo pacman -S --noconfirm ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav 
 
+echo "==> Instalando pikaur..."
+sudo pacman -S --needed --noconfirm base-devel git
+git clone https://aur.archlinux.org/pikaur.git
+cd pikaur
+makepkg -fsri --noconfirm
+cd ..
+rm -rf pikaur
+
 echo "==> Instalando Shader Booster..."
 git clone https://github.com/psygreg/shader-patcherx.git
 cd shader-patcherx
