c3bcd20823c697f5c94da7d7e389e0149182fea3
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-09-02 13:56:01 -0300

    Update t.sh

--- t.sh
+++ t.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "==> Ativando firewall (GUFW)..."
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
-sudo ufw enable
-
 echo "==> Atualizando sistema e instalando microcódigo Intel..."
 sudo pacman -Scc --noconfirm
 echo "==> Script finalizado com sucesso; instale o nvm se quiser depois!"
 cd ..
 rm -rf Arch
-exit
-reboot
-
