7e4abff0ff3a3d866c6f8078aab0e0b1fdf7df96
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-09-02 10:19:57 -0300

    Update teste.sh

--- teste.sh
+++ teste.sh
@@ -9,5 +9,6 @@ sudo ufw enable
 echo "==> Atualizando sistema e instalando microcódigo Intel..."
 sudo pacman -Scc --noconfirm
 echo "==> Script finalizado com sucesso; instale o nvm se quiser depois!"
+cd..
 rm -rf Arch
 
