6d96a5c577734ddfe04961f461aa398a7754ec51
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-09-06 10:24:08 -0300

    Update s.sh

--- s.sh
+++ s.sh
@@ -21,7 +21,6 @@ echo "==> Ativando firewall (GUFW)..."
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo systemctl enable ufw
-sudo ufw enable
 
 echo "==> Instalando Chaotic-AUR..."
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
