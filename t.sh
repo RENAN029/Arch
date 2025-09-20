bea7805d07f5923c555140821bf660797bda8c3a
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-09-20 14:32:16 -0300

    Update t.sh

--- t.sh
+++ t.sh
@@ -1,20 +1,25 @@
 set -e
 
-sudo apt install --noconfirm gnome-shell gnome-console gnome-software gnome-tweaks gnome-control-center gnome-disk-utility gnome-system-monitor
+sudo apt install -y gnome-shell gnome-console gnome-software gnome-tweaks gnome-control-center gnome-disk-utility gnome-system-monitor
 
-sudo apt install --noconfirm ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer
+sudo apt install -y ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer
 
-sudo apt install --noconfirm gdm3 nvidia-open nano fastfetch gufw noto-fonts-cjk fwupd flatpak intel-ucode yt-dlp aria2
+sudo apt install -y gdm3 nvidia-open nano fastfetch gufw noto-fonts-cjk fwupd flatpak intel-ucode yt-dlp aria2
 
 systemctl enable gdm3
+
 git clone https://github.com/maximilionus/lucidglyph.git
 cd lucidglyph
 sudo ./lucidglyph.sh install
 cd ..
 rm -rf lucidglyph
 
-sudo pacman -Syu --noconfirm 
-sudo pacman -Scc --noconfirm
+sudo sed -i 's/trixie/testing/g' /etc/apt/sources.list
+
+sudo apt full-upgrade -y
+sudo autoremove --purge -y
+sudo apt clean
+
 echo "==> Script finalizado com sucesso; instale o nvm, shader booster e o aur helper se quiser depois!"
 cd ..
 rm -rf Arch
