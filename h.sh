f82aa7b89000c128f5cf00b6c8ea56b8a51bc0ef
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-09-23 14:23:24 -0300

    Update h.sh

--- h.sh
+++ h.sh
@@ -4,7 +4,7 @@ sudo pacman -S --noconfirm lightdm-gtk-greeter hyprland nautilus wofi waybar kit
 
 sudo pacman -S --noconfirm ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer swaync
 
-sudo pacman -S --noconfirm btop nvidia-open nano fastfetch gufw noto-fonts-cjk fwupd flatpak intel-ucode yt-dlp aria2 hyprpaper
+sudo pacman -S --noconfirm intel-ucode nvidia-open nano btop fastfetch gufw noto-fonts-cjk fwupd flatpak yt-dlp aria2 hyprpaper
 
 systemctl enable lightdm
 git clone https://github.com/maximilionus/lucidglyph.git
