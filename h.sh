c6f378bb33fbba3756c62588e58f05aa51f14eaa
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-09-24 08:16:47 -0300

    Update h.sh

--- h.sh
+++ h.sh
@@ -6,9 +6,10 @@ sudo pacman -S --noconfirm ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-
 
 sudo pacman -S --noconfirm intel-ucode nvidia-open fwupd flatpak fastfetch btop ufw noto-fonts-cjk yt-dlp aria2 hyprpaper
 
-flatpak bazar
+flatpak install flathub io.github.kolunmi.Bazaar
 
 systemctl enable ly
+
 git clone https://github.com/maximilionus/lucidglyph.git
 cd lucidglyph
 sudo ./lucidglyph.sh install
@@ -19,6 +20,7 @@ sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U --noconfirm "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst"
 sudo pacman -U --noconfirm "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst"
+
 sudo sed -i 's/^#Color/Color/' /etc/pacman.conf
 sudo sed -i '/Color/a ILoveCandy' /etc/pacman.conf
 sudo sed -i '/^ParallelDownloads/d' /etc/pacman.conf
@@ -27,6 +29,7 @@ echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee
 
 sudo pacman -Syu --noconfirm 
 sudo pacman -Scc --noconfirm
+
 echo "==> Script finalizado com sucesso; instale o nvm, shader booster e o aur helper se quiser depois!"
 cd ..
 rm -rf Arch
