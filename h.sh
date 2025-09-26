b1f5be6aad2aa938372aa895e5edda875d65d618
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-09-26 15:13:35 -0300

    Update h.sh

--- h.sh
+++ h.sh
@@ -2,7 +2,7 @@ set -e
 
 sudo pacman -S --noconfirm ly hyprland kitty wofi waybar superfile dunst xdg-desktop-portal-hyprland hyprpolkitagent dunst
 
-sudo pacman -S --noconfirm ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer
+sudo pacman -S --noconfirm ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer discover
 
 sudo pacman -S --noconfirm uwsm nvidia-open intel-ucode nano fastfetch btop ufw noto-fonts-cjk fwupd flatpak yt-dlp aria2
 
