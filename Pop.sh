bfb4afd0b0eb347ca21d2c76508df77a2cb5a9ea
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-09-26 19:34:29 -0300

    Create Pop.sh

---
+++ Pop.sh
@@ -0,0 +1,26 @@
+set -e
+
+sudo pacman -S --noconfirm lightdm-gtk-greeter hyprland kitty wofi ncdu hyprpaper hyprpolkitagent xdg-desktop-portal-hyprland ttf-firacode-nerd    
+sudo pacman -S --noconfirm ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer gnome-software nautilus 
+sudo pacman -S --noconfirm uwsm nvidia-open intel-ucode nano fastfetch btop ufw noto-fonts-cjk fwupd flatpak yt-dlp aria2 swaync hyprshot waybar 
+git clone https://github.com/maximilionus/lucidglyph.git
+cd lucidglyph
+sudo ./lucidglyph.sh install
+cd ..
+rm -rf lucidglyph
+
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+sudo pacman -U --noconfirm "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst"
+sudo pacman -U --noconfirm "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst"
+sudo sed -i 's/^#Color/Color/' /etc/pacman.conf
+sudo sed -i '/Color/a ILoveCandy' /etc/pacman.conf
+sudo sed -i '/^ParallelDownloads/d' /etc/pacman.conf
+sudo sed -i '/ILoveCandy/a ParallelDownloads = 15' /etc/pacman.conf
+echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
+
+sudo pacman -Syu --noconfirm 
+sudo pacman -Scc --noconfirm
+echo "==> Script finalizado com sucesso; instale o nvm, shader booster e o aur helper se quiser depois!"
+cd ..
+rm -rf Arch
