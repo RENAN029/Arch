cb30649d4d2f5dc22649c187cc58ccd3fa150908
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-09-27 14:48:03 -0300

    Update h.sh

--- h.sh
+++ h.sh
@@ -1,8 +1,8 @@
 set -e
 
-sudo pacman -S --noconfirm ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer gnome-software hyprland    
-sudo pacman -S --noconfirm uwsm nvidia-open intel-ucode superfile micro fastfetch btop gufw noto-fonts-cjk fwupd flatpak yt-dlp aria2 hyprpaper
-sudo pacman -S --noconfirm ly hyprshot ncdu swaync kitty wofi waybar hyprpolkitagent xdg-desktop-portal-hyprland xdg-user-dirs ttf-firacode-nerd 
+sudo pacman -S --noconfirm ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer gnome-software hyprpicker    
+sudo pacman -S --noconfirm ly uwsm nvidia-open intel-ucode superfile micro fastfetch btop ufw noto-fonts-cjk fwupd flatpak yt-dlp aria2 hyprpaper
+sudo pacman -S --noconfirm hyprshot swaync kitty wofi waybar hyprpolkitagent xdg-desktop-portal-hyprland xdg-user-dirs ttf-firacode-nerd hyprland
 
 systemctl enable ly
 git clone https://github.com/maximilionus/lucidglyph.git
