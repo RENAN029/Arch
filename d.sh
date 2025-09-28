ef8628619b58a16a7703d013c0e505d2b3a56b83
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-09-28 12:42:37 -0300

    Update d.sh

--- d.sh
+++ d.sh
@@ -2,7 +2,7 @@ set -e
 
 sudo pacman -S --noconfirm gnome-shell gnome-console gnome-software gnome-tweaks gnome-control-center gnome-disk-utility 
 sudo pacman -S --noconfirm ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer
-sudo pacman -S --noconfirm gdm nvidia-open intel-ucode nano fastfetch btop gufw noto-fonts-cjk fwupd flatpak yt-dlp aria
+sudo pacman -S --noconfirm gdm nvidia-open intel-ucode nano fastfetch btop gufw noto-fonts-cjk fwupd flatpak yt-dlp aria2
 
 systemctl enable gdm
 git clone https://github.com/maximilionus/lucidglyph.git
