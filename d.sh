36e8842da1e59e78a32438b00b8b35b53b0cb2c4
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-10-02 14:31:18 -0300

    Update d.sh

--- d.sh
+++ d.sh
@@ -15,7 +15,7 @@ sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U --noconfirm "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst"
 sudo pacman -U --noconfirm "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst"
-
+sudo pacman -S --noconfirm earlyoom ffpegthumbnailer
 sudo sed -i 's/^#Color/Color/' /etc/pacman.conf
 sudo sed -i '/Color/a ILoveCandy' /etc/pacman.conf
 sudo sed -i '/^ParallelDownloads/d' /etc/pacman.conf
