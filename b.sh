7c5e229797b5572587b65d2aaf6c4a4202c3a804
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-10-02 14:29:35 -0300

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,5 +1,5 @@
 set -e
-sudo pacman -S earlyoom ffpegthumbnailer
+sudo pacman -S earlyoom ffpegthumbnailer nix
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo systemctl enable ufw
