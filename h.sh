f439b7985e5a31653edd084a3b2a77540d33499f
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-10-11 08:11:11 -0300

    Update h.sh

--- h.sh
+++ h.sh
@@ -1,7 +1,7 @@
 set -e
 sudo pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-noto-nerd noto-fonts-extra ttf-jetbrains-mono
 sudo pacman -S --noconfirm ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer 
-sudo pacman -S --noconfirm hyprland hyprpaper hyprpicker kitty rofi waybar superfile earlyoom mplayer termusic ufw dunst
+sudo pacman -S --noconfirm hyprland hyprpaper hyprpicker hypridle kitty rofi waybar superfile mpv earlyoom termusic gufw 
 sudo pacman -S --noconfirm nvidia-open intel-ucode micro fastfetch btop fwupd flatpak yt-dlp aria2 hyprshot calcurse feh
 sudo pacman -S --noconfirm lightdm-gtk-greeter xdg-user-dirs xdg-desktop-portal-hyprland hyprpolkitagent uwsm presenterm 
  
