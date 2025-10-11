41cff4f72f6f8682fb17d5ab18c9b216fb0ee1fb
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-10-11 07:13:46 -0300

    Update h.sh

--- h.sh
+++ h.sh
@@ -17,12 +17,12 @@ sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U --noconfirm "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst"
 sudo pacman -U --noconfirm "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst"
-flatpak install -y flathub io.github.kolunmi.Bazaar app.zen_browser.zen org.onlyoffice.desktopeditors  
 sudo sed -i 's/^#Color/Color/' /etc/pacman.conf
 sudo sed -i '/Color/a ILoveCandy' /etc/pacman.conf
 sudo sed -i '/^ParallelDownloads/d' /etc/pacman.conf
 sudo sed -i '/ILoveCandy/a ParallelDownloads = 15' /etc/pacman.conf
 echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
+
 sudo pacman -Syu --noconfirm 
 sudo pacman -Scc --noconfirm
 echo "==> Script finalizado com sucesso; instale o nvm, git, shader booster e o aur helper se quiser depois!"
