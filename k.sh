0adcb78d7e4ad5c6eb1f2be49fabd2b0517f0821
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-10-12 08:25:06 -0300

    Update k.sh

--- k.sh
+++ k.sh
@@ -4,7 +4,6 @@ sudo pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-noto-n
 sudo pacman -S --noconfirm ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer 
 sudo pacman -S --noconfirm ark nvidia-open intel-ucode git micro fastfetch btop ufw fwupd flatpak yt-dlp aria2 earlyoom
 sudo pacman -S --noconfirm plasma-desktop dolphin konsole discover kscreen partitionmanager sddm-kcm plasma-firewall
-
 systemctl enable sddm earlyoom
 git clone https://github.com/maximilionus/lucidglyph.git
 cd lucidglyph
