8ab7a4594721d5cdae4b00e75a09252daf142ac8
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-10-12 06:20:16 -0300

    Update k.sh

--- k.sh
+++ k.sh
@@ -3,7 +3,7 @@ set -e
 sudo pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-noto-nerd noto-fonts-extra ttf-jetbrains-mono
 sudo pacman -S --noconfirm ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer 
 sudo pacman -S --noconfirm sddm nvidia-open intel-ucode git micro fastfetch btop ufw fwupd flatpak yt-dlp aria2 earlyoom
-sudo pacman -S --noconfirm plasma-desktop kde-utilities-meta kde-system-meta kde-sdk-meta kde-pim-meta kde-network-meta 
+sudo pacman -S --noconfirm plasma-desktop dolophin konsole discover kscreen kdeconnect partitionmanager
 
 systemctl enable sddm earlyoom
 git clone https://github.com/maximilionus/lucidglyph.git
