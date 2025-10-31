8b740dfbe31287a5c3f17e1ed129bb7e4da259bd
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-10-31 10:10:38 -0300

    Update b.sh

--- b.sh
+++ b.sh
@@ -13,12 +13,12 @@ git clone https://github.com/psygreg/shader-patcherx.git
 cd shader-patcherx
 chmod +x patcher.sh
 ./patcher.sh
-
 cd ..
 rm -rf shader-patcherx
 
 sudo pacman -Scc --noconfirm
 sudo pacman -Syu --noconfirm
+
 echo "==> zen; hydra; office"
 
 cd ..
