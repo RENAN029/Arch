37db4db722a45a96c7ecdd8cb224a78b8165fdf5
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-11-07 08:12:30 -0300

    Update h.sh

--- h.sh
+++ h.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+set -e
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U --noconfirm "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst"
