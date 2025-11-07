d0286833eb64416ffca9aeda2c8183f254ee2e60
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-11-07 08:09:02 -0300

    Update h.sh

--- h.sh
+++ h.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+sudo pacman -U --noconfirm "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst"
+sudo pacman -U --noconfirm "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst"
+sudo sed -i 's/^#Color/Color/' /etc/pacman.conf
+sudo sed -i '/Color/a ILoveCandy' /etc/pacman.conf
+sudo sed -i '/^ParallelDownloads/d' /etc/pacman.conf
+sudo sed -i '/ILoveCandy/a ParallelDownloads = 15' /etc/pacman.conf
+echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
+sudo pacman -S --noconfirm nvidia-open intel-ucode ufw fwupd gamemode fuse apparmor flatpak earlyoom
+
 flatpak install flathub io.github.kolunmi.Bazaar
 echo "=== Instalador de Dots Hyprland ==="
 
