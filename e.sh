544b42d69d131e19b9f2a6d37e7500f2e1a32424
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-11-10 15:08:57 -0300

    Update e.sh

--- e.sh
+++ e.sh
@@ -4,6 +4,8 @@ sudo pacman -U --noconfirm "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-ke
 sudo pacman -U --noconfirm "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst"
 echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
 sudo pacman -S --noconfirm nvidia-open intel-ucode ufw fwupd gamemode fuse apparmor flatpak earlyoom
-flatpak install -y flathub io.github.kolunmi.Bazaar
+sudo pacman -S hyprpaper hyprpicker xdg-user-dirs hypridle hyprlock hyprqt6engine hyprpolkitagent ly 
+sudo pacman -S hyprsysteminfo hyprsunset hyprlauncher hyprland-qt-support xdg-desktop-portal-hyprland  
+flatpak install -y flathub io.github.kolunmi.Bazaar 
 systemctl enable apparmor earlyoom
 
