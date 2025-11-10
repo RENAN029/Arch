d5523a0c4055fb0b3f12faa5f73bb5cfc8bc660c
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-11-10 14:57:33 -0300

    Update e.sh

--- e.sh
+++ e.sh
@@ -1,1 +1,9 @@
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+sudo pacman -U --noconfirm "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst"
+sudo pacman -U --noconfirm "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst"
+echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
+sudo pacman -S --noconfirm nvidia-open intel-ucode ufw fwupd gamemode fuse apparmor flatpak earlyoom
+flatpak install -y flathub io.github.kolunmi.Bazaar
+systemctl enable apparmor earlyoom
 
