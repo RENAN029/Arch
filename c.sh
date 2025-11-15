349428a41ce8c60a504d8c3100e85abc57b4bf32
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-11-15 09:03:26 -0300

    Update c.sh

--- c.sh
+++ c.sh
@@ -2,7 +2,7 @@ set -e
 sudo pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-noto-nerd noto-fonts-extra ttf-jetbrains-mono
 sudo pacman -S --noconfirm ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer 
 sudo pacman -S --noconfirm cosmic-session cosmic-files cosmic-store cosmic-terminal cosmic-text-editor cosmic-player gparted
-sudo pacman -S --noconfirm nvidia-open intel-ucode git helix fastfetch btop gufw fwupd flatpak yt-dlp aria2 earlyoom vscodium 
+sudo pacman -S --noconfirm nvidia-open intel-ucode git micro fastfetch btop gufw fwupd flatpak yt-dlp aria2 earlyoom vscodium 
 
 sudo pacman -S --noconfirm gamemode fuse dnsmasq apparmor 
 git clone https://github.com/maximilionus/lucidglyph.git
