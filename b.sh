5a935cb7308e4847acac9499e905eb7198080ff2
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-12-06 17:00:53 -0300

    Add echo commands for installation scripts
    Added commands to echo installation scripts for various tools.

--- b.sh
+++ b.sh
@@ -3,23 +3,23 @@ sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo systemctl enable ufw 
 sudo ufw enable
-
 sudo ufw allow 53317/udp
 sudo ufw allow 53317/tcp
 sudo ufw allow 1714:1764/udp
 sudo ufw allow 1714:1764/tcp
 sudo ufw reload
-
 git clone https://github.com/psygreg/shader-patcherx.git
 cd shader-patcherx 
 chmod +x patcher.sh
 ./patcher.sh
-
 cd ..
 rm -rf shader-patcherx
 sudo pacman -Scc --noconfirm
 sudo pacman -Syu --noconfirm
-
+echo "curl -fsSL https://install.danklinux.com | sh"
+echo "sh <(curl -L https://raw.githubusercontent.com/JaKooLit/Hyprland-Dots/main/Distro-Hyprland.sh)"
+echo "bash <(curl -s https://ii.clsty.link/get)"
+echo "curl -fsSL get.axeni.de/ax-shell | bash"
 echo "FIM"
 cd ..
 rm -rf Arch
