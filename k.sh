ea3f02e5b50234bbf01e1eb362b3dbaec4a4903e
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-12-06 07:46:06 -0300

    Update k.sh

--- k.sh
+++ k.sh
@@ -12,7 +12,6 @@ cd lucidglyph
 sudo ./lucidglyph.sh install
 cd ..
 rm -rf lucidglyph
-
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U --noconfirm "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst"
@@ -23,7 +22,8 @@ sudo sed -i '/^ParallelDownloads/d' /etc/pacman.conf
 sudo sed -i '/ILoveCandy/a ParallelDownloads = 15' /etc/pacman.conf
 echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
 echo "==> plasma-vault; plasma-systemmonitor; plasma-thunderbolt; oxygen; print-manager; ffmpegthumbs; dolphin-plugins"
-
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
 sudo pacman -Syu --noconfirm 
 sudo pacman -Scc --noconfirm
 cd ..
