d6ac0f0e80e82f93425bf43e99af9d869d992101
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-12-06 07:48:28 -0300

    Update c.sh to include LazyVim installation

--- c.sh
+++ c.sh
@@ -11,20 +11,20 @@ cd lucidglyph
 sudo ./lucidglyph.sh install
 cd ..
 rm -rf lucidglyph
-
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U --noconfirm "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst"
 sudo pacman -U --noconfirm "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst"
-
 sudo sed -i 's/^#Color/Color/' /etc/pacman.conf
 sudo sed -i '/Color/a ILoveCandy' /etc/pacman.conf
 sudo sed -i '/^ParallelDownloads/d' /etc/pacman.conf
 sudo sed -i '/ILoveCandy/a ParallelDownloads = 15' /etc/pacman.conf
 echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
 
-echo "==> zen; gimp; lutris; hydralauncher; prismlauncher; steam; kdenlive; protonplus; onlyoffice; localsend; vscodium"
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
 sudo pacman -Syu --noconfirm 
 sudo pacman -Scc --noconfirm
+echo "==> zen; gimp; lutris; hydralauncher; prismlauncher; steam; kdenlive; protonplus; onlyoffice; localsend; vscodium"
 cd ..
 rm -rf Arch
