7396f71df5aceea9591015df0aedd747784ffb37
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-12-20 17:44:46 -0300

    Add installation command for Oh My Bash

--- b.sh
+++ b.sh
@@ -17,6 +17,7 @@ rm -rf shader-patcherx
 sudo pacman -Scc --noconfirm
 sudo pacman -Syu --noconfirm
 echo "curl -fsSL https://install.danklinux.com | sh"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 echo "sudo pacman -S zoxide; bat; net-tools; bind"
 echo "sh <(curl -L https://raw.githubusercontent.com/JaKooLit/Hyprland-Dots/main/Distro-Hyprland.sh)"
 echo "bash <(curl -s https://ii.clsty.link/get)"
