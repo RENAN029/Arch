a844fa9f2383a0ba2ee0f50c842fd596b681cd0a
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-12-21 17:52:21 -0300

    Update b.sh

--- b.sh
+++ b.sh
@@ -21,7 +21,7 @@ bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/mast
 
 sudo pacman -Scc --noconfirm
 sudo pacman -Syu --noconfirm
-echo "bash <(curl -s https://ii.clsty.link/get)"
+echo "curl -fsSL https://install.danklinux.com | sh"
 echo "sh <(curl -L https://raw.githubusercontent.com/JaKooLit/Hyprland-Dots/main/Distro-Hyprland.sh)"
 echo "bash <(curl -s https://ii.clsty.link/get)"
 sudo ufw status
