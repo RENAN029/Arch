7c51d001a42368c3221967f27b3c9671b42d20bd
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-12-23 08:14:10 -0300

    Update k.sh

--- k.sh
+++ k.sh
@@ -1,4 +1,5 @@
 set -e
+
 sudo pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-noto-nerd noto-fonts-extra ttf-jetbrains-mono
 sudo pacman -S --noconfirm ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer
 sudo pacman -S --noconfirm ark nvidia-open intel-ucode git neovim fastfetch btop ufw fwupd flatpak yt-dlp aria2 earlyoom  
@@ -24,6 +25,6 @@ echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee
 
 sudo pacman -Syu --noconfirm 
 sudo pacman -Scc --noconfirm
-echo "==> plasma-vault; plasma-systemmonitor; plasma-disks; kdeplasma-addons; drkonqi; ffmpegthumbs; dolphin-plugins"
+echo "==> https://library.hydra.wiki/library; https://github.com/psygreg/linuxtoys; https://github.com/imputnet/helium"
 cd ..
 rm -rf Arch
