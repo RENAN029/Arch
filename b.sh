8fd6b09e55d2411058c8d984188e74db78d4f8a9
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-12-25 13:17:34 -0300

    Update b.sh

--- b.sh
+++ b.sh
@@ -17,7 +17,6 @@ rm -rf shader-patcherx
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
-echo "==> fish; zsh; superfile; eza; fzf; zoxide; bat; tldr; ; duf; ldns; fd; ripgrep; tailscale"
  
 sudo pacman -Scc --noconfirm
 sudo pacman -Syu --noconfirm
