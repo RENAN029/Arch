574b3bd309307e98b454adb642340020bf0552c6
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-12-25 10:53:48 -0300

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,4 +1,5 @@
 set -e
+
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo systemctl enable ufw  
@@ -17,13 +18,12 @@ cd ..
 rm -rf shader-patcherx
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
+echo -e "\neval "$(starship init bash)"" | sudo tee -a ~/.bashrc
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
 sudo pacman -Scc --noconfirm
 sudo pacman -Syu --noconfirm
-echo "curl -fsSL https://install.danklinux.com | sh"
-echo "sh <(curl -L https://raw.githubusercontent.com/JaKooLit/Hyprland-Dots/main/Distro-Hyprland.sh)"
-echo "bash <(curl -s https://ii.clsty.link/get)"
+fastfetch
 sudo ufw status
 echo "FIM"
 cd ..
