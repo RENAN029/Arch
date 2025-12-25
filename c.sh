5c3cfd916aee9b86334a37546af3bd8d1a53317c
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-12-25 09:45:33 -0300

    Update c.sh

--- c.sh
+++ c.sh
@@ -2,8 +2,8 @@ set -e
 
 sudo pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-noto-nerd noto-fonts-extra ttf-jetbrains-mono
 sudo pacman -S --noconfirm ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer
-sudo pacman -S --noconfirm nvidia-open intel-ucode neovim fastfetch btop ufw fwupd flatpak yt-dlp aria2 earlyoom openvpn 
-sudo pacman -S --noconfirm cosmic-session cosmic-terminal cosmic-files cosmic-store xdg-user-dirs smartmontools git ncdu  
+sudo pacman -S --noconfirm cosmic-session cosmic-terminal cosmic-files cosmic-store xdg-user-dirs smartmontools git ncdu
+sudo pacman -S --noconfirm nvidia-open intel-ucode neovim fastfetch btop ufw fwupd flatpak yt-dlp aria2 earlyoom starship 
 
 sudo pacman -S --noconfirm gamemode fuse dnsmasq apparmor 
 git clone https://github.com/maximilionus/lucidglyph.git
@@ -25,6 +25,6 @@ echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee
 
 sudo pacman -Syu --noconfirm 
 sudo pacman -Scc --noconfirm
-echo "==> nix; yay; ananicy-cpp; clamav; irqbalance; pnpm; mise; cups; mangohud; gamescope; starship; distrobox; docker"        
+echo "==> nix; yay; ananicy-cpp; clamav; irqbalance; pnpm; mise; cups; mangohud; gamescope; tailscale; distrobox; docker"       
 cd ..
 rm -rf TAILSCALE
