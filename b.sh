1c5802aef3d1e6c28d81e219a06436d33e736ea0
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-12-26 12:55:17 -0300

    Update b.sh

--- b.sh
+++ b.sh
@@ -16,7 +16,7 @@ cd ..
 rm -rf shader-patcherx
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
-echo "sudo pacman - S fish starship ripgrep zoxide tldr ldns fzf eza duf bat fd" 
+echo "sudo pacman - S fish starship ripgrep zoxide tldr ldns fzf eza duf bat fd gping" 
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
 sudo pacman -Scc --noconfirm
