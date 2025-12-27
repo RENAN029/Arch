23b78b680d799545dec3dab4b39d23dbf79036b6
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-12-27 09:49:50 -0300

    Update c.sh

--- c.sh
+++ c.sh
@@ -4,7 +4,7 @@ sudo pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-noto-n
 sudo pacman -S --noconfirm ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer
 sudo pacman -S --noconfirm nvidia-open intel-ucode neovim fastfetch btop ufw fwupd flatpak yt-dlp aria2 earlyoom git gdu 
 sudo pacman -S --noconfirm cosmic-session cosmic-terminal cosmic-files cosmic-store xdg-user-dirs gnome-keyring tailscale
-echo "sudo pacman -S nix yay ananicy-cpp clamav irqbalance pnpm mise cups mangohud gamescope distrobox docker iwd"
+echo "sudo pacman -S nix yay ananicy-cpp clamav irqbalance pnpm mise cups mangohud gamescope distrobox docker httpie iwd"
 sudo pacman -S --noconfirm gamemode fuse dnsmasq apparmor 
 git clone https://github.com/maximilionus/lucidglyph.git
 systemctl enable cosmic-greeter earlyoom dnsmasq apparmor 
